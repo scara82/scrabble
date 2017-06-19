dc9cb0c802125af28de5c5ee78991f3e0c3e7b01
Marta Scaramella <marta.scaramella@gmail.com>
2017-06-19 13:34:54 +1000

    test one, score with reduce not working

--- scrabble.rb
+++ scrabble.rb
@@ -1,9 +1,6 @@
-require 'pry'
-
-class Scrabble
-
+require "pry"
 # hash with the letters points
-  letters_score = {
+@@letters_points = {
   "A" => 1, "B" => 3, "C" => 3, "D" => 2,
   "E" => 1, "F" => 4, "G" => 2, "H" => 4,
   "I" => 1, "J" => 8, "K" => 5, "L" => 1,
@@ -11,11 +8,19 @@ class Scrabble
   "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
   "U" => 1, "V" => 4, "W" => 4, "X" => 8,
   "Y" => 4, "Z" => 10
-  }
+}
 
-  def score(word)
-    word = word.upcase
-    return word
+class Scrabble
+
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    tot_score = 0
+    tot_score = @@letters_points.values_at(* @word.upcase.chars)
+    tot_score.reduce(0, :+)
+      return tot_score
   end
 end
 
--- scrabble_test.rb
+++ scrabble_test.rb
@@ -5,5 +5,8 @@ require_relative "scrabble"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ScrabbleTest < MiniTest::Unit::TestCase
-
+  def test_score_return_points
+    game = Scrabble.new('hello').score
+    assert_equal "8", game
+  end
 end
