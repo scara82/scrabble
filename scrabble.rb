11bb4ba83d18048c389c28e9487c068f8f974ff9
Marta Scaramella <marta.scaramella@gmail.com>
2017-08-08 11:40:34 +1000

    eliminated unecessary return for methods

--- scrabble.rb
+++ scrabble.rb
@@ -16,7 +16,7 @@ class Scrabble
 
     # check if there are special characters or numbers in the word
     elsif word =~ /[^a-zA-Z]/
-      return 'invalid input, please enter only letters'
+      'invalid input, please enter only letters'
 
     # the word's letters are compared with the hash key and the corresponding value
     # is added at the counter (tot_score) with the reduce method.
@@ -24,12 +24,12 @@ class Scrabble
     # All letters are converted in upcase to match the hash keys.
     else
       tot_score = @@letters_points.values_at(*word.upcase.chars).reduce(0, :+)
-        return tot_score
+        # return tot_score
     end
   end
 
   def is_the_letter_into_the_word?(word, letter)
-    return ((n = word.count letter) > 0)? "#{n} times" : 'no, the letter is not in the word'
+    ((n = word.count letter) > 0)? "#{n} times" : 'no, the letter is not in the word'
   end
 
   def letter_score(letter)
@@ -49,7 +49,7 @@ class Scrabble
     # if zero a "invalid letter" messagge will be returned otherwise will return
     # the basic score of the word, plus the letter value one time (because one times
     # has already been counted in score()) plus n times the letter is in the word)
-    return ((n = word.count letter) > 0)? (score(word) + n * letter_score(letter)) : 'invalid letter'
+    ((n = word.count letter) > 0)? (score(word) + n * letter_score(letter)) : 'invalid letter'
   end
 
   def triple_letter(word,letter)
@@ -57,11 +57,11 @@ class Scrabble
     # if zero a "invalid letter" messagge will be returned otherwise will return
     # the basic score of the word, plus the letter value two time (because one times
     # has already been counted in score()) plus n times the letter is in the word)
-    return ((n = word.count letter) > 0)? (score(word) + 2 * n * letter_score(letter)) : 'invalid letter'
+    ((n = word.count letter) > 0)? (score(word) + 2 * n * letter_score(letter)) : 'invalid letter'
   end
 
   def n_times_letter(word, letter, times)
-    return ((n = word.count letter) > 0)? (score(word) + (times - 1) * n * letter_score(letter)) : 'invalid letter'
+    ((n = word.count letter) > 0)? (score(word) + (times - 1) * n * letter_score(letter)) : 'invalid letter'
   end
 
 end
